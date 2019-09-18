ee052d09ec3ada22fc7e71659c12cbbef8276046
gurjeet <gurjit.babrah@gmail.com>
2019-09-18 18:19:34 -0400

    r2-Green

--- src/SoftwareSales.java
+++ src/SoftwareSales.java
@@ -3,9 +3,23 @@ public class SoftwareSales {
 
 	public double calculateQuantity(int q)
 	{
-		return 99.0*q;
+
+		
+		double subTotal= 99.0*q;
+        double discount=0.0;
+        if (q >= 10 && q <= 19) {
+
+			discount = subTotal * 0.20;
+		}
+		
+		double finalTotal = subTotal - discount;
+		return finalTotal;
+		
+	}
+	
+
 		
 	}
 	
 	
-}
+
