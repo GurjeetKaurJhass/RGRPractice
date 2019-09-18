187f3e25c4a2b0de77ced168903561d45eb32bbc
gurjeet <gurjit.babrah@gmail.com>
2019-09-18 13:50:16 -0400

    initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,22 @@
+
+public class EvenOdd {
+
+	public boolean isEven(int n)
+	{
+		if (n <= 0) {
+			
+			return false;
+		}
+		else {
+			
+			if (n % 2 == 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	
+	
+	
+	}}
---
+++ tests/EvenOdd.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EvenOdd {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		System.out.println("Initial");
+		System.out.println("Initial");
+
+		
+		System.out.println("Initdsdsdsfsdfdial");
+		System.out.println("Initial");
+
+		
+}}
